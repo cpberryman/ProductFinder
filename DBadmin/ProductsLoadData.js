b1eccafbbf51ba8ac79e3e4c0492b832c083b7e5
Chris Berryman <c.p.berryman@btinternet.com>
2015-11-25 15:30:50 +0000

    Database administration files

---
+++ DBadmin/ProductsLoadData.js
@@ -0,0 +1,43 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+
+console.log("Importing movies into DynamoDB. Please wait.");
+
+var allProducts = JSON.parse(fs.readFileSync('products.json', 'utf8'));
+allProducts.forEach(function(product) {
+    var params = {
+        TableName: "Products",
+        Item: {
+
+            "TitleText": product.Title.TitleText,
+            "ProductIdentifier":  product.ProductIdentifier,
+            "Contributor": product.Contributor,
+            "Language": product.Language,
+            "AudienceCode": product.AudienceCode,
+            "Imprint": product.Imprint,
+            "Publisher": product.Publisher,
+            "PublishingStatus": product.PublishingStatus,
+            "PublicationDate": product.PublicationDate,
+            "SalesRights": product.SalesRights,
+            "SupplyDetail": product.SupplyDetail,
+            "PpmData": product.PpmData,
+            "chapters": product.chapters
+
+        }
+    };
+
+    dynamodbDoc.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add product", product.Title.TitleText, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:", product.Title.TitleText);
+       }
+    });
+});
