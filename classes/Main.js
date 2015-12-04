5c101050f198eac1c1e34be7c787df1bdb35c59d
Chris Berryman <c.p.berryman@btinternet.com>
2015-12-04 14:11:32 +0000

    Updated controller method calls

--- classes/Main.js
+++ classes/Main.js
@@ -2,8 +2,9 @@
  * This class handles events from the user
  *
  * Modification history
- * Version	Modifier	Date		Change				 Reason
- * 0.1.0	Chris		25-11-2015	First release	 Requirements
+ * Version	Modifier	Date		Change				               Reason
+ * 0.1.0	Chris		25-11-2015	First release	               Requirements
+ * 0.2.0	Chris		04-11-2015	updated method invocations	 Requirements
  */
 var main = function() {
 
@@ -12,11 +13,11 @@ var c = new Controller();
   $('#search-button').click(function() {
     $('.product').remove();
     var searchTerm =  $('#search-text').val();
-    c.searchForTitle(searchTerm);
+    c.findProduct(searchTerm);
   });
 
   $('#browse-link').click(function() {
-    c.listTitles();
+     c.listProducts();
   });
 
 }
